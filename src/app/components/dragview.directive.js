741f358964786864ff84ce373d7c215f7bd9e450
Heikki Pesonen <heikki.pesonen@luotta.fi>
2015-06-16 18:16:07 +0300

    improved dragview

--- src/app/components/dragview.directive.js
+++ src/app/components/dragview.directive.js
@@ -2,17 +2,21 @@
 
 angular.module('imgurapp')
 
-	.directive('dragView', function(){
+	.directive('dragView', function($q, $timeout){
 		return {
 			restrict:'A',
 			link:function($scope, $element){
 				var el = $element[0];
-				var offset = 0;
-				var deltax = 0;
-				var deltay = 0;
+				var offset = {x:0, y:0};
+				var delta = {x:0, y:0};
 				var lastEvent = false;
 				var width = null;
 
+				/**
+				 * get cursor position from touch and mouse event
+				 * @param  {event} evt
+				 * @return {object} {x: int, y: int}
+				 */
 				function getCursor(evt){
 					if (evt.touches.length > 0){
 						return {x:evt.touches[0].pageX,y:evt.touches[0].pageY};
@@ -21,18 +25,43 @@ angular.module('imgurapp')
 					}
 				}
 
+				/**
+				 * set element position (translate)
+				 * @param {int} duration animation duration in milliseconds
+				 */
 				function setPosition(duration){
+					var d = $q.defer();
+
 					el.style.transition = duration ? duration +'ms' : '';
-					el.style.transform = 'translate3d('+offset+'px,0,0)';
-					el.style['-webkit-transform'] = 'translate3d('+offset+'px,0,0)';
+					el.style.transform = 'translate3d('+offset.x+'px,0,0)';
+					el.style['-webkit-transform'] = 'translate3d('+offset.x+'px,0,0)';
+
+					$timeout(function(){
+						d.resolve(offset.x);
+					}, duration || 1);
+
+					return d.promise;
 				}
 
 
+
+				/**
+				 * draggin started
+				 * @param  {mouse event} evt
+				 * @return {void}
+				 */
 				function dragStart(evt){
 					width = _.parseInt( window.getComputedStyle(el).width );
 					lastEvent = getCursor(evt);
+					delta.x = 0;
+					delta.y = 0;
 				}
 
+				/**
+				 * dragging is performed
+				 * @param  {mouseevent} evt
+				 * @return {void}
+				 */
 				function dragMove(evt){
 					var currentPosition = getCursor(evt);
 
@@ -40,25 +69,31 @@ angular.module('imgurapp')
 					var stepy = currentPosition.y - lastEvent.y;
 
 
-					deltax += stepx;
-					deltay += stepy;
+					delta.x += stepx;
+					delta.y += stepy;
 
-					if (Math.abs(deltax) > deltay){
+					if (Math.abs(delta.x) > delta.y){
 						evt.stopPropagation();
 						evt.preventDefault();
 
-						offset += stepx;
+						offset.x += stepx;
 						setPosition();
 					}
 
 					lastEvent = currentPosition;
 				}
 
+
+				/**
+				 * drag ends (mouseup, touchend)
+				 * @param  {mouseevent} evt
+				 * @return {void}
+				 */
 				function dragEnd(evt){
-					var movedRatio = Math.abs(offset) / width;
+					var movedRatio = Math.abs(offset.x) / width;
 					console.log(movedRatio);
 
-					offset = 0;
+					offset.x = 0;
 					setPosition(400);
 				}
 
@@ -66,6 +101,12 @@ angular.module('imgurapp')
 				el.addEventListener('touchstart', dragStart);
 				el.addEventListener('touchmove', dragMove);
 				el.addEventListener('touchend', dragEnd);
+
+				$scope.$on('$destroy', function(){
+					el.removeEventListener('touchstart', dragStart);
+					el.removeEventListener('touchmove', dragMove);
+					el.removeEventListener('touchend', dragEnd);
+				});
 			}
 		};
 	});
