888328b6920d24a6f85f3cab3dcb4d1900d2229a
Heikki Pesonen <heikki.pesonen@luotta.fi>
2015-06-25 00:55:44 +0300

    improved comments displaying

--- src/app/components/image.comments.directive.js
+++ src/app/components/image.comments.directive.js
@@ -13,6 +13,7 @@ angular.module('imgurapp')
 			'<div class="comment">'+
 				'<div class="comment-author" ng-bind="::comment.author"></div>'+
 				'<div class="comment-comment" ng-bind="::comment.comment"></div>'+
+				'<div class="comment-children" ng-bind="::\'replies: \'+comment.children.length"></div>'+
 			'</div>'
 		};
 	});
