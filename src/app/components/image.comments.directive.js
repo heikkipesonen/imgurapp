a75ff8c63677eeff0da23706f94d3f6398f246af
Heikki Pesonen <heikki.pesonen@luotta.fi>
2015-06-25 15:26:07 +0300

    image comments section development

--- src/app/components/image.comments.directive.js
+++ src/app/components/image.comments.directive.js
@@ -9,11 +9,23 @@ angular.module('imgurapp')
 			scope:{
 				comment:'='
 			},
+			controller:function CommentController($scope){
+				$scope.childVisible = false;
+				$scope.showChildren =function (){
+					$scope.childVisible = !$scope.childVisible;
+				};
+			},
 			template:
-			'<div class="comment">'+
+			'<div class="comment" ng-click="showChildren()">'+
 				'<div class="comment-author" ng-bind="::comment.author"></div>'+
 				'<div class="comment-comment" ng-bind="::comment.comment"></div>'+
 				'<div class="comment-children" ng-bind="::\'replies: \'+comment.children.length"></div>'+
+
+				'<div class="comment-child" ng-if="childVisible" ng-repeat="(childIndex, child) in comment.children track by childIndex">'+
+					'<div class="comment-author" ng-bind="::child.author"></div>'+
+					'<div class="comment-comment" ng-bind="::child.comment"></div>'+
+					'<div class="comment-children" ng-bind="::\'replies: \'+child.children.length"></div>'+
+				'</div>'+
 			'</div>'
 		};
 	});
