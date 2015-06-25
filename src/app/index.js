d2dd20d7da1c84099b74e1d631ffe999ef3f0e2b
Heikki Pesonen <heikki.pesonen@luotta.fi>
2015-06-26 01:22:40 +0300

    removed unused config variables

--- src/app/index.js
+++ src/app/index.js
@@ -5,9 +5,7 @@ angular.module('imgurapp', ['angular-loading-bar','ngAnimate', 'ngCookies', 'ngT
 	.constant('appConfig', {
 		api:'https://api.imgur.com/3',
 		client_id: 'b84e33b2ff595f6',
-		client_secret: '8ebf546494d982fdd5b9c4155218ab1ddd06cbbd',
-    gridItemSize: window.innerWidth / 4,
-    gridItemGutter: 10
+		client_secret: '8ebf546494d982fdd5b9c4155218ab1ddd06cbbd'
 	})
 
   .config(function(imgurApiProvider, appConfig) {
